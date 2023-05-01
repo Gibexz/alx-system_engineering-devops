0824c18e027899e9648720f10bdfe0fb53b04c2f
Gibexz <andrewetedu@gmail.com>
2023-05-01 15:03:05 +0100

    Adv Task 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * infinite_while - function that containts an infinite loop
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main -vEntry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	pid_t pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork error");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
