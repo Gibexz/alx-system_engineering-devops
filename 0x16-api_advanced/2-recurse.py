6a6f9c072e23d8c6d6b0674ad87b5a95d30a0d98
Gibexz <andrewetedu@gmail.com>
2023-08-09 00:12:32 +0100

    Task 2 updates

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,4 +6,32 @@ module: 2-recurse.py
 import requests
 
 
-def recurse(subreddit, hot_list=[])
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    function that recursively queries the Reddit API and prints
+    the titles of the hot posts listed for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    if after:
+        url = url + f"?after={after}"
+    header = {"User-Agent": "Gibexz"}
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
+    dataa = response.json()
+    if "data" in dataa and "children" in dataa["data"]:
+        posts_dict = dataa["data"]["children"]
+
+        for post in posts_dict:
+            hot_list.append(post["data"]["title"])
+
+        after = dataa["data"]["after"]
+        if after:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+
+    else:
+        return None
